9176f3f6dccc620e4a240996fec896fde614a0f5
maxexq <parames.ajh@gmail.com>
2026-02-15 21:33:03 +0700

    feat: add statCard coponent

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,16 @@
 import { MainLayout } from "@/components/layout";
 import { TestToastButton } from "@/components";
+import StatCard from "@/components/atoms/StatCard";
+import { Badge, User, XCircle } from "lucide-react";
 
 const Home = () => {
   return (
     <MainLayout>
+      <div className="flex items-center justify-center gap-8.75">
+        <StatCard icon={User} title="Stars" value="100" color="yellow" />
+        <StatCard icon={Badge} title="Likes" value="50" color="red" />
+        <StatCard icon={XCircle} title="Comments" value="20" color="blue" />
+      </div>
       <TestToastButton />
     </MainLayout>
   );
